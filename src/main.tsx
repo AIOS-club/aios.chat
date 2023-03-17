4003c7fe8953b46aaea912f70e726f1493a1e9d6
yi yi <cjjj3845@gmail.com>
2023-03-17 12:48:05 +0800

    chore:删除无用代码

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { SWRConfig } from 'swr';
 import App from './app';
 import './index.css';
 
@@ -9,18 +8,8 @@ const BASE = import.meta.env.BASE_URL;
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
-    <SWRConfig
-      value={{
-        revalidateOnFocus: false,
-        revalidateIfStale: true,
-        shouldRetryOnError: false,
-        revalidateOnMount: false,
-        errorRetryCount: 0
-      }}
-    >
-      <Router basename={BASE}>
-        <App />
-      </Router>
-    </SWRConfig>
+    <Router basename={BASE}>
+      <App />
+    </Router>
   </React.StrictMode>
 );
