b59002a1c0a301d5f4769a2f98418e3171d99cba
yi yi <cjjj3845@gmail.com>
2023-04-14 00:30:30 +0800

    fix: stream disabled

--- src/components/config-setting/index.tsx
+++ src/components/config-setting/index.tsx
@@ -42,7 +42,7 @@ const ConfigSetting: React.FC<ConfigSettingProps> = function ConfigSetting(props
           '-2': '-2', '-1': '-1', 0: '0', 1: '1', 2: '2' 
         }}
       />
-      <Form.Switch field="stream" initValue={config.stream || true} />
+      <Form.Switch field="stream" initValue={config.stream || true} disabled />
     </Form>
   );
 };
