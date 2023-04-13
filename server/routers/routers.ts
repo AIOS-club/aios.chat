2326a8c8f3110a493129650d327090659143d3ea
yi yi <cjjj3845@gmail.com>
2023-04-14 00:21:51 +0800

    feat: 支持所有参数修改

--- server/routers/routers.ts
+++ server/routers/routers.ts
@@ -1,57 +1,76 @@
 import Router from 'koa-router';
-import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from 'openai';
-import { getSystemMessage, parseStreamText, ErrorMessage } from '../utils';
+import { OpenAIApi, ChatCompletionRequestMessage } from 'openai';
+import { parseStreamText, ErrorMessage } from '../utils';
 import env from '../config';
 
 const RETRIES = parseInt(env.RETRIES, 10) ?? 3;
 
 interface Body {
   messages: ChatCompletionRequestMessage[];
-  apiKey?: string;
+  frequency_penalty?: number;
+  model?: string;
+  presence_penalty?: number;
+  temperature?: number;
+  stream?: boolean;
+}
+
+interface CallOpenAIProps {
+  openai: OpenAIApi;
+  messages: ChatCompletionRequestMessage[];
+  retries: number;
+  authorization?: string;
+  config: Record<string, any>;
 }
 
 const router = new Router();
 
-const callOpenAI = async (
-  openai: OpenAIApi,
-  messages: ChatCompletionRequestMessage[],
-  retries: number
-): Promise<any> => {
+const callOpenAI = async ({
+  openai,
+  messages,
+  retries,
+  config,
+  authorization = '',
+}: CallOpenAIProps): Promise<any> => {
   try {
     const res = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
-      messages: [...getSystemMessage(), ...messages],
+      messages,
       temperature: 0.8,
       n: 1, // 限制ai的回答数
       presence_penalty: -1.0, // 不轻易改变对话主题
       frequency_penalty: 1.0, // 减少重复已提及的内容
       stream: true,
+      ...config,
       // stop: []
       // max_tokens: 1000,
-    }, { responseType: 'stream' }) as any;
+    }, { responseType: 'stream', headers: { authorization } }) as any;
 
     return res;
   } catch (error) {
     if (retries === 0) throw error;
-    return callOpenAI(openai, messages, retries - 1);
+    return callOpenAI({
+      openai, messages, retries: retries - 1, authorization, config 
+    });
   }
 };
 
 router.post('/aios-chat', async (ctx) => {
   const body = ctx.request.body as Body;
+  const { authorization } = ctx.request.headers;
+  const auth = authorization || `Bearer ${env.API_KEY}`;
 
-  const { messages, apiKey } = body;
+  const { messages, ...config } = body;
 
   ctx.set('Cache-Control', 'no-cache');
   ctx.set('Content-Type', 'text/event-stream');
   ctx.set('Connection', 'keep-alive');
 
   try {
-    const config = new Configuration({ apiKey: apiKey || env.API_KEY });
+    const openai = new OpenAIApi();
 
-    const openai = new OpenAIApi(config);
-
-    const res = await callOpenAI(openai, messages, RETRIES);
+    const res = await callOpenAI({
+      openai, messages, authorization: auth, config, retries: RETRIES 
+    });
 
     ctx.status = 200;
 
