86db024968646c1714669cd0164f20477173be9d
yi yi <cjjj3845@gmail.com>
2023-04-20 21:35:33 +0800

    fix: fix env error

--- api/src/utils/env.ts
+++ api/src/utils/env.ts
@@ -2,21 +2,29 @@ import * as dotenv from 'dotenv';
 import fs from 'fs';
 
 function loadEnv() {
-  if (process.env.NODE_ENV === 'production') {
+  try {
+    if (process.env.NODE_ENV === 'production') {
+      dotenv.config();
+    } else {
+      dotenv.config({ path: './api/.env.development' });
+    }
+  } catch {
     dotenv.config();
-  } else {
-    dotenv.config({ path: './api/.env.development' });
   }
 }
 
 loadEnv();
 
 if (process.env.NODE_ENV !== 'production') {
-  fs.watch('./api/.env.development', (eventType, filename) => {
-    if (eventType === 'change' && filename === '.env.development') {
-      loadEnv();
-    }
-  });
+  try {
+    fs.watch('./api/.env.development', (eventType, filename) => {
+      if (eventType === 'change' && filename === '.env.development') {
+        loadEnv();
+      }
+    });
+  } catch {
+    
+  }
 }
 
 export default process.env;
