c2f3f15403d8124011629ca9f850f19d27ded5a2
yi yi <cjjj3845@gmail.com>
2023-05-08 15:11:34 +0800

    imp: 自动滚动到底部

--- src/hooks/useScrollToBottom.ts
+++ src/hooks/useScrollToBottom.ts
@@ -1,13 +1,14 @@
-import { useCallback, useRef } from 'react';
+import { useRef } from 'react';
+import throttle from 'lodash/throttle';
 
 function useScrollToBottom(): [React.MutableRefObject<any>, () => void] {
   const ref = useRef<HTMLElement>();
 
-  const scrollToBottom = useCallback(() => {
+  const scrollToBottom = throttle(() => {
     if (ref.current?.scrollTo && typeof ref.current.scrollTo === 'function') {
       ref.current.scrollTo?.({ left: 0, top: ref.current.scrollHeight, behavior: 'smooth' });
     }
-  }, []);
+  }, 1000, { leading: true, trailing: false });
 
   return [ref, scrollToBottom];
 }
